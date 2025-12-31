6eb093590ab79c82ec063bdea4ec3a5131c73441
AtanasG6 <gyulchev666@gmail.com>
2026-01-01 00:43:59 +0200

    Add TreeNode generic class and create sample tree structure

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Trees
+﻿namespace Trees;
+
+public class Program
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        TreeNode<int> c1 = new TreeNode<int>(1),
+                      c2 = new TreeNode<int>(12),
+                      c3 = new TreeNode<int>(31);
+
+        TreeNode<int> c4 = new TreeNode<int>(23),
+                      c5 = new TreeNode<int>(6);
+
+        TreeNode<int> b1 = new TreeNode<int>(19, c1, c2, c3),
+                      b2 = new TreeNode<int>(21),
+                      b3 = new TreeNode<int>(14, c4, c5);
+
+        TreeNode<int> a1 = new TreeNode<int>(7, b1, b2, b3);
+
     }
 }
---
+++ Trees/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Trees;
+
+public class TreeNode<T>
+{
+    public T Value { get; set; }
+    public IReadOnlyCollection<TreeNode<T>> Children { get; set; }
+
+    public TreeNode(T value, params TreeNode<T>[] children)
+    {
+        this.Value = value;
+        this.Children = children.AsReadOnly();
+    }
+}
