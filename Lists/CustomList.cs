58b20c7233cbc98128dbd0e96effc82f25f7dc72
AtanasG6 <gyulchev666@gmail.com>
2026-01-01 17:31:48 +0200

    Add indexer with validation and make Add/Grow methods private

--- Lists/CustomList.cs
+++ Lists/CustomList.cs
@@ -11,7 +11,21 @@ public class CustomList
         Count = 0;
     }
 
-    public void Add(int element)
+    public int this[int index]
+    {
+        get
+        {
+            this.ValidateIndex(index);
+            return this._array[index];
+        }
+        set
+        {
+            this.ValidateIndex(index);
+            this._array[index] = value;
+        }
+    }
+
+    private void Add(int element)
     {
         if (this.Count == this._array.Length)
             this.Grow();
@@ -20,10 +34,16 @@ public class CustomList
         this.Count++;
     }
 
-    public void Grow()
+    private void Grow()
     {
         int[] newArray = new int[this._array.Length * 2];
         Array.Copy(this._array, newArray, this._array.Length);
         this._array = newArray;
+    }
+
+    private void ValidateIndex(int index)
+    {
+        if (index < 0 || index >= this.Count)
+            throw new IndexOutOfRangeException("Index was out of range.");
     }
 }
